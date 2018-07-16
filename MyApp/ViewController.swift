339299e86e86a8078b622fc802181ec4bf452d8c
Bhushan Bapat <bhushanbapat@bhushans-macbook-pro.local>
2018-07-16 08:46:39 +0530

    Initial Commit

---
+++ MyApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MyApp
+//
+//  Created by Bhushan Bapat on 16/07/18.
+//  Copyright © 2018 Bhushan Bapat. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
